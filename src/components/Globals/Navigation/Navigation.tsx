7df6e58e343f06aada8e803aa859cc54b749b770
Morgan <morganramsey08@gmail.com>
2026-04-21 19:18:11 -0500

    Update Navigation.tsx

--- src/components/Globals/Navigation/Navigation.tsx
+++ src/components/Globals/Navigation/Navigation.tsx
@@ -1,38 +1,15 @@
 import Link from "next/link";
-import { print } from "graphql/language/printer";
-
 import styles from "./Navigation.module.css";
 
-import { MenuItem, RootQueryToMenuItemConnection } from "@/gql/graphql";
-import { fetchGraphQL } from "@/utils/fetchGraphQL";
-import gql from "graphql-tag";
-
-async function getData() {
-  const menuQuery = gql`
-    query MenuQuery {
-      menuItems(where: { location: PRIMARY_MENU }) {
-        nodes {
-          uri
-          target
-          label
-        }
-      }
-    }
-  `;
-
-  const { menuItems } = await fetchGraphQL<{
-    menuItems: RootQueryToMenuItemConnection;
-  }>(print(menuQuery));
-
-  if (menuItems === null) {
-    throw new Error("Failed to fetch data");
-  }
-
-  return menuItems;
-}
-
+// We're turning this into a simple component that doesn't 
+// crash the build if WordPress is missing a menu.
 export default async function Navigation() {
-  const menuItems = await getData();
+  
+  // For now, we'll use a hardcoded menu so your site can deploy.
+  // Once the site is live, we can reconnect this to WordPress properly.
+  const manualNavItems = [
+    { label: "Home", uri: "/" },
+  ];
 
   return (
     <nav
@@ -41,20 +18,15 @@ export default async function Navigation() {
       itemScope
       itemType="http://schema.org/SiteNavigationElement"
     >
-      {menuItems.nodes.map((item: MenuItem, index: number) => {
-        if (!item.uri) return null;
-
-        return (
-          <Link
-            itemProp="url"
-            href={item.uri}
-            key={index}
-            target={item.target || "_self"}
-          >
-            <span itemProp="name">{item.label}</span>
-          </Link>
-        );
-      })}
+      {manualNavItems.map((item, index) => (
+        <Link
+          itemProp="url"
+          href={item.uri}
+          key={index}
+        >
+          <span itemProp="name">{item.label}</span>
+        </Link>
+      ))}
     </nav>
   );
 }
